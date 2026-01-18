ba8c81ffa463ce6cab661aa831809e2774c8a38a
Boso Mac <robertoingrao4@gmail.com>
2026-01-18 09:48:26 +0100

    selfwork operatori 1

---
+++ Selfworkoperatori1.js
@@ -0,0 +1,16 @@
+//Crea 5 variabili contenenti un numero e scrivi un programma in modo da ottenere la somma tra questi numeri e la media.In console poi mostra la seguente frase: ‘La somma tra i numeri equivale a … e la media equivale a…
+
+
+let num1 = 5 ;
+let num2 = 3 ;
+let num3 = 10 ;
+let num4 = 15 ;
+let num5 = 25 ;
+let dividendo_media = 5 ;
+
+
+let somma = ( num1 + num2 + num3 + num4 + num5 )
+
+let media = ( somma / dividendo_media )
+
+console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
